83ad4447d2ffe2d67c9a6588c691cfb4850b1ace
Tetsuya Mori <monry84@gmail.com>
2018-02-07 22:06:53 +0900

    :sparkles: Support to base class interface changes

--- Assets/Scripts/CAFU/Routing/Presentation/View/Controller.cs
+++ Assets/Scripts/CAFU/Routing/Presentation/View/Controller.cs
@@ -1,12 +1,13 @@
-﻿using CAFU.Core.Presentation.Presenter;
-using CAFU.Core.Presentation.View;
+﻿using CAFU.Core.Presentation.View;
 using CAFU.Routing.Presentation.Presenter;
 using UnityEngine;
 using UnityModule;
+// ReSharper disable ArrangeAccessorOwnerBody
+// ReSharper disable UnusedMember.Global
 
-namespace CAFU.Routing.Presentation.View {
+namespace CAFU.Routing.Presentation.View.Launcher {
 
-    public class Controller : Controller<LauncherPresenter, LauncherPresenter.Factory> {
+    public class Controller : Controller<Controller, LauncherPresenter, LauncherPresenter.Factory> {
 
         [SerializeField][SceneName]
         private string initialScene;
@@ -26,7 +27,7 @@ namespace CAFU.Routing.Presentation.View {
     public static class ViewExtension {
 
         public static LauncherPresenter GetPresenter(this IView view) {
-            return ControllerInstanceManager.Instance.Get(view.GetType().Namespace).Presenter.As<LauncherPresenter>();
+            return Controller.Instance.Presenter;
         }
 
     }
