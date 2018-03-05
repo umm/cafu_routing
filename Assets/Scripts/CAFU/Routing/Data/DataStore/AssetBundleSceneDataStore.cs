1b346b2399d0f81485281c104dd4242eea836f55
Tetsuya Mori <monry84@gmail.com>
2018-03-05 10:57:56 +0900

    :sparkles: Implement AssetBundleSceneDataStore
    * AssetBundle からシーンを読み込む DataStore を実装
      * これにより、既存の SceneDataStore は Obsolete になり、 InBuildSceneDataStore に名称変更されました

---
+++ Assets/Scripts/CAFU/Routing/Data/DataStore/AssetBundleSceneDataStore.cs
@@ -0,0 +1,26 @@
+﻿using CAFU.Core.Data.DataStore;
+using CAFU.Routing.Data.Entity;
+using UniRx;
+using UnityEngine.SceneManagement;
+using UnityModule.AssetBundleManagement;
+using UnityModule.ContextManagement;
+
+namespace CAFU.Routing.Data.DataStore {
+
+    public class AssetBundleSceneDataStore : SceneDataStoreBase {
+
+        public class Factory : DefaultDataStoreFactory<AssetBundleSceneDataStore> {
+
+        }
+
+        public override IObservable<SceneEntity> LoadSceneAsObservable(string sceneName, LoadSceneMode loadSceneMode) {
+            // 先に AssetBundle からシーンを読み込む
+            UnityEngine.Debug.Log(ContextManager.CurrentProject.As<IDownloadableProjectContext>());
+            return Loader.GetInstance(ContextManager.CurrentProject.As<IDownloadableProjectContext>())
+                .LoadAssetAsObservable<SceneObject>(sceneName)
+                .SelectMany(_ => base.LoadSceneAsObservable(sceneName, loadSceneMode));
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/CAFU/Routing/Data/DataStore/InBuildSceneDataStore.cs
@@ -0,0 +1,13 @@
+﻿using CAFU.Core.Data.DataStore;
+
+namespace CAFU.Routing.Data.DataStore {
+
+    public class InBuildSceneDataStore : SceneDataStoreBase {
+
+        public class Factory : DefaultDataStoreFactory<InBuildSceneDataStore> {
+
+        }
+
+    }
+
+}
