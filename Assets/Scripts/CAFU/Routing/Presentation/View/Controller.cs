9c642f8964045c17f8b05ff86c6035883db24091
Tetsuya Mori <monry84@gmail.com>
2018-03-06 11:16:59 +0900

    :fire: Remove old class
    * Obsolete で使用禁止を伝えたいところだが、Add Component されるモノなので伝える手段がない

--- Assets/Scripts/CAFU/Routing/Presentation/View/Controller.cs
+++ Assets/Scripts/CAFU/Routing/Presentation/View/Controller.cs
@@ -1,13 +1,13 @@
 ﻿using CAFU.Core.Presentation.View;
 using CAFU.Routing.Presentation.Presenter;
 using UnityEngine;
-using UnityModule;
+
 // ReSharper disable ArrangeAccessorOwnerBody
 // ReSharper disable UnusedMember.Global
 
 namespace CAFU.Routing.Presentation.View.Launcher {
 
-    public abstract class Controller<TSceneName> : Controller<Controller, LauncherPresenter, LauncherPresenter.Factory> where TSceneName : struct {
+    public abstract class Controller<TSceneName> : Controller<Controller<TSceneName>, LauncherPresenter, LauncherPresenter.Factory> where TSceneName : struct {
 
         [SerializeField]
         private TSceneName initialScene;
@@ -20,31 +20,6 @@ namespace CAFU.Routing.Presentation.View.Launcher {
 
         protected override void Start() {
             this.GetPresenter().LaunchInitialScene(this.InitialScene);
-        }
-
-    }
-
-    public class Controller : Controller<Controller, LauncherPresenter, LauncherPresenter.Factory> {
-
-        [SerializeField][SceneName]
-        private string initialScene;
-
-        private string InitialScene {
-            get {
-                return this.initialScene;
-            }
-        }
-
-        protected override void Start() {
-            this.GetPresenter().LaunchInitialScene(this.InitialScene);
-        }
-
-    }
-
-    public static class ViewExtension {
-
-        public static LauncherPresenter GetPresenter(this IView view) {
-            return Controller.Instance.Presenter;
         }
 
     }
