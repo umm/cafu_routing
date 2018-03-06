ac4cdfb008ef6cb060dbd3a61178e90e7217cdbf
Tetsuya Mori <monry84@gmail.com>
2018-03-06 11:13:56 +0900

    :sparkles: Support enum argument in Launcher Controller
    * 各プロジェクトで自前実装した Controller から起動出来るようにする
    * こうしないと、ProjectContext を用いる AssetBundle 起動が巧く動作しない

--- Assets/Scripts/CAFU/Routing/Presentation/View/Controller.cs
+++ Assets/Scripts/CAFU/Routing/Presentation/View/Controller.cs
@@ -7,6 +7,23 @@ using UnityModule;
 
 namespace CAFU.Routing.Presentation.View.Launcher {
 
+    public abstract class Controller<TSceneName> : Controller<Controller, LauncherPresenter, LauncherPresenter.Factory> where TSceneName : struct {
+
+        [SerializeField]
+        private TSceneName initialScene;
+
+        private TSceneName InitialScene {
+            get {
+                return this.initialScene;
+            }
+        }
+
+        protected override void Start() {
+            this.GetPresenter().LaunchInitialScene(this.InitialScene);
+        }
+
+    }
+
     public class Controller : Controller<Controller, LauncherPresenter, LauncherPresenter.Factory> {
 
         [SerializeField][SceneName]
